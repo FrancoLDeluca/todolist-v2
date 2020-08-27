c65b8dd75bfbf226deb555f559101c74b958dbc4
Franco <frandeluca22@gmail.com>
2020-08-26 23:21:03 -0500

    First commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-franco:ykU2OEaiBDTYZ6iV@cluster0.jbsip.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
@@ -129,3 +129,6 @@ app.get("/about", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+//Pass: ykU2OEaiBDTYZ6iV
